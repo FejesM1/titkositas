a8b62ed8d3d5c9785a888ef18bcd3606a46995e7
FejesM <fejesm@kkszki.hu>
2025-10-16 12:54:54 +0200

    Kész a program

--- progtetel2/Program.cs
+++ progtetel2/Program.cs
@@ -10,8 +10,43 @@ namespace progtetel2
     {
         static void Main(string[] args)
         {
-            int kod = 5827;
-            string kod_szoveg = " ";
+            Console.WriteLine("Adjon meg egy négyjegyű kódot: ");
+            int kod = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Adjon meg egy szöveget: ");
+            string szoveg = Console.ReadLine();
+
+            Console.WriteLine($"A(z) {szoveg} szöveg titkosítva:{titkosito(kod,szoveg)}");
+
+        }
+        static string titkosito(int kod, string kodolando)
+        {
+            string abc = "aábceéfghijklmnoóöőpqrstuúüűvwxyzaábceéfghijklmnoóöőpqrstuúüűvwxyz";
+            string titkositott = " ";
+            string kodsz = kod.ToString();
+            int index = 0;
+            int szovegin = 0;
+            int szam = 0;
+            for (int i = 0; i < kodolando.Length; i++)
+            {
+                if (!abc.Contains(kodolando[i]))
+                {
+                    continue;
+                }
+                
+                szam = int.Parse(kodsz[index].ToString());
+                
+                szovegin = abc.IndexOf(kodolando[i]);
+
+                titkositott += abc[szovegin + szam];
+                if (index == 3)
+                {
+                   index = -1;
+                }
+                index++;
+            }
+            
+
+            return titkositott;
         }
     }
 }
