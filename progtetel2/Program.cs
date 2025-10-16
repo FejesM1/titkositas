4d1bdb48c824772131300a138306c630c3352cae
FejesM <fejesm@kkszki.hu>
2025-10-16 11:02:45 +0200

    Add project files.

---
+++ progtetel2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace progtetel2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int kod = 5827;
+            string kod_szoveg = " ";
+        }
+    }
+}
